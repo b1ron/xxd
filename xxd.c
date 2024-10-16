f0942ba38a1e3dc383ba7e8a2c7e766d89f1ea83
b1ron <goldingbyron@gmail.com>
2024-10-17 01:47:39 +0200

    add file

---
+++ xxd.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+
+int main() 
+{    
+    setlocale(LC_CTYPE, "");
+    wchar_t ch = 0x52;
+    // wprintf(L"%lc\n", ch); // needed to set locale later.
+
+    int quotient, remainder;
+
+    char str[] = "http://4pt4axjgz";
+    int length = sizeof(str);
+    for (int i = 0; i < length-1; ++i)
+    {
+        quotient = str[i] / 16;
+        remainder = str[i] % 16;
+        printf("%x%x\n", quotient, remainder);
+    }
+
+    return 0;
+}
