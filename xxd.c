881b5c4fdd4111a5c28ce75f9f0cae989cd75380
b1ron <goldingbyron@gmail.com>
2024-10-18 00:34:01 +0200

    second hex value

--- xxd.c
+++ xxd.c
@@ -2,6 +2,7 @@
 #include <wchar.h>
 #include <locale.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
@@ -11,16 +12,24 @@ int main()
 
     int quotient, remainder;
 
-    char str[] = "https://abcd3434"; // 00000000: 6874 7470 733a 2f2f 6162 6364 3334 3334  https://abcd3434
+    char input[] = "https://abcd3434"; // 00000000: 6874 7470 733a 2f2f 6162 6364 3334 3334  https://abcd3434
 
     char *s_builder = malloc(4 * sizeof(char));
-    for (int i = 0; i < sizeof(str) - 1; ++i)
+    for (int i = 0; i < sizeof(input) - 2; i = i + 2)
     {
-        quotient = str[i] / 16;
-        remainder = str[i] % 16;
+        // first hex value
+        quotient = input[i] / 16;
+        remainder = input[i] % 16;
 
-        sprintf(&s_builder[i], "%x%x ", quotient, remainder);
-        printf("%x%x ", s_builder[i], s_builder[i + 1]);
+        sprintf(s_builder, "%x%x", quotient, remainder);
+        printf("%s", s_builder);
+
+        // second hex value
+        quotient = input[i + 1] / 16;
+        remainder = input[i + 1] % 16;
+
+        sprintf(s_builder, "%x%x ", quotient, remainder);
+        printf("%s", s_builder);
     }
 
     printf("\n");
