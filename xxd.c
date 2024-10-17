5ea8cd261e595016a56333f511c6e2badcc02799
b1ron <goldingbyron@gmail.com>
2024-10-17 17:13:29 +0200

    format

--- xxd.c
+++ xxd.c
@@ -2,30 +2,25 @@
 #include <wchar.h>
 #include <locale.h>
 
-int main() 
-{    
+int main()
+{
     setlocale(LC_CTYPE, "");
-    wchar_t ch = 0x52;
+    wchar_t ch = 0x52; // for later use
     // wprintf(L"%lc\n", ch);
 
     int quotient, remainder;
 
     char str[] = "https://abcd3434"; // 00000000: 6874 7470 733a 2f2f 6162 6364 3334 3334  https://abcd3434
-    int length = sizeof(str);
-    for (int i = 0; i < length-1; ++i)
+
+    int len = sizeof(str);
+    for (int i = 0; i < len - 1; ++i)
     {
         quotient = str[i] / 16;
         remainder = str[i] % 16;
-        printf("%x%x", quotient, remainder);
+        printf("%x%x ", quotient, remainder);
     }
 
-    int c;
-
-    c = getchar();
-    while (c != EOF) {
-        putchar(c);
-        c = getchar();
-    }
+    printf("\n");
 
     return 0;
 }
