e5f4747b3df346724be289c411679d4bdf41ea15
b1ron <goldingbyron@gmail.com>
2024-10-18 13:56:00 +0200

    fopen and read stream from fp

--- xxd.c
+++ xxd.c
@@ -12,6 +12,22 @@ int main()
     wchar_t ch = 0x52;
     // wprintf(L"%lc\n", ch);
 
+    // TODO: read filename from stdin
+    FILE *fp;
+    fp = fopen("file", "r");
+    if (fp == NULL)
+    {
+        printf("Error opening file\n");
+        exit(1);
+    }
+
+    // read file
+    int c;
+    while ((c = getc(fp)) != EOF)
+    {
+        wprintf(L"%lc", c);
+    }
+
     int quotient, remainder;
 
     char input[] = "https://abcd3434"; // 00000000: 6874 7470 733a 2f2f 6162 6364 3334 3334  https://abcd3434
